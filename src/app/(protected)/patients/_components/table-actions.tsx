d259a1d74a612af8cdebfdcbc9234abf3076631d
Leandro Amaral <lramaral02m@gmail.com>
2025-06-17 18:09:32 -0300

    feat: ensure patient can be updated

---
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -0,0 +1,48 @@
+import { Edit2Icon, MoreVertical, TrashIcon } from "lucide-react";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+
+import UpsertPatientForm from "./upsert-patient-form";
+
+interface PatientsTableActionsProps {
+  patient: typeof patientsTable.$inferSelect;
+}
+
+const PatientsTableActions = ({ patient }: PatientsTableActionsProps) => {
+  const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
+  return (  
+    <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button variant="ghost" size="icon">
+                <MoreVertical className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
+                <Edit2Icon />
+                Editar
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <TrashIcon />
+                Excluir
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <UpsertPatientForm isOpen={upsertDialogIsOpen} 
+            patient={patient}
+            onSuccess={() => setUpsertDialogIsOpen(false)}
+            />
+        </Dialog>
+  );
+}
+ 
+export default PatientsTableActions;
